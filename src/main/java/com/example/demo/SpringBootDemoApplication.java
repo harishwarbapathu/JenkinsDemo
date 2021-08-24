840127b346a1e866236b07859a02b1669545ec48
harishwarbapathu <harishwar2512@gmail.com>
2021-08-24 15:32:20 +0530

    third commit

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -9,6 +9,7 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		System.out.println("Checking github and Jenkins");
 	}
 
 }
